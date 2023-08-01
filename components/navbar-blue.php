90c086b685141b4be665cfd28db79d9a4d0247c5
Cairo <cairofelipedev@gmail.com>
2023-08-01 18:20:46 -0300

    Add links

--- components/navbar-blue.php
+++ components/navbar-blue.php
@@ -6,17 +6,17 @@
       </a>
       <div class="flex items-center lg:order-2">
         <div class="nav_icons px-4 flex space-x-2">
-          <a class="bg-color1 p-1.5 rounded-full w-10 h-10 text-center text-white text-lg hidden lg:block" href="#">
+          <a class="bg-color1 p-1.5 rounded-full w-10 h-10 text-center text-white text-lg hidden lg:block" href="https://api.whatsapp.com/send/?phone=5586981590855&text=Ol%C3%A1%2C+bom+dia%21+&type=phone_number&app_absent=0">
             <i class="bi bi-whatsapp"></i>
           </a>
-          <a class="bg-color1 p-1.5 rounded-full w-10 h-10 text-center text-white text-lg hidden lg:block" href="#">
+          <a class="bg-color1 p-1.5 rounded-full w-10 h-10 text-center text-white text-lg hidden lg:block" href="https://www.instagram.com/nortelink.sistemas/">
             <i class="bi bi-instagram"></i>
           </a>
         </div>
-        <a class="bg-color1 p-1.5 mr-1 rounded-full w-10 h-10 text-center text-white text-lg lg:hidden" href="#">
+        <a class="bg-color1 p-1.5 mr-1 rounded-full w-10 h-10 text-center text-white text-lg lg:hidden" href="https://api.whatsapp.com/send/?phone=5586981590855&text=Ol%C3%A1%2C+bom+dia%21+&type=phone_number&app_absent=0">
           <i class="bi bi-whatsapp"></i>
         </a>
-        <a class="bg-color1 p-1.5 rounded-full w-10 h-10 text-center text-white text-lg lg:hidden" href="#">
+        <a class="bg-color1 p-1.5 rounded-full w-10 h-10 text-center text-white text-lg lg:hidden" href="https://www.instagram.com/nortelink.sistemas/">
           <i class="bi bi-instagram"></i>
         </a>
       </div>
